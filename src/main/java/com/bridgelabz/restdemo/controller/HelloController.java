216310a47a61c0acb559114d0f3d15b185689c5d
mehulbhange <bhangemehul@gmail.com>
2022-10-06 14:40:05 +0530

    ADDED:[Mehul] hello controller with get request mapping with query parameters

--- src/main/java/com/bridgelabz/restdemo/controller/HelloController.java
+++ src/main/java/com/bridgelabz/restdemo/controller/HelloController.java
@@ -1,14 +1,21 @@
 package com.bridgelabz.restdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+    //get request mapping
     @GetMapping("/hello")
     public String hello(){
         return "Hello from Bridgelabz";
+    }
+
+    //get request mapping with query parameter
+    @GetMapping("/helloParam")
+    public String hello(@RequestParam String name){
+        return "Hello "+ name +" from Bridgelabz";
     }
 
 }
