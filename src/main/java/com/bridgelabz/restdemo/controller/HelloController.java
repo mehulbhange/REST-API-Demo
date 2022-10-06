0976329ac88ad1f27bdd3a78f1a81b5e3305c0b8
mehulbhange <bhangemehul@gmail.com>
2022-10-06 14:46:06 +0530

    ADDED:[Mehul] get request mapping with path variable

--- src/main/java/com/bridgelabz/restdemo/controller/HelloController.java
+++ src/main/java/com/bridgelabz/restdemo/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.restdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,12 @@ public class HelloController {
     public String hello(@RequestParam String name){
         return "Hello "+ name +" from Bridgelabz";
     }
+
+    //get request mapping with path variable
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name){
+        return "Hello "+ name +" from Bridgelabz";
+    }
+
 
 }
